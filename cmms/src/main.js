a863f93dde92083b967144d66a075df1d4486927
Censyu <censyu@outlook.com>
2020-08-04 14:46:21 +0800

    Add logic for cas login status check

--- cmms/src/main.js
+++ cmms/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Axios from "axios";
 import VueAxios from "vue-axios";
 import router from "./router.js";
+import VueCookies from "vue-cookies";
 import Vuetify from "vuetify/lib";
 import VuetifyConfirm from "vuetify-confirm";
 import Vuex from "vuex";
@@ -13,6 +14,7 @@ import App from "./App.vue";
 
 Vue.use(Vuex);
 Vue.use(VueAxios, Axios);
+Vue.use(VueCookies);
 Vue.use(mavonEditor);
 Vue.use(DatetimePicker);
 
@@ -27,9 +29,12 @@ Vue.config.productionTip = false;
 // global navigation guards (before)
 router.beforeEach((to, from, next) => {
     console.log(to);
-    if (window.sessionStorage.getItem("token")) {
+    if (router.app.$cookies.get("login") === "true") {
         if (to.name === "Login") next({ path: "/home" });
-        else next();
+        else {
+            console.log("here");
+            next();
+        }
     } else {
         if (to.name === "Login" || to.meta.needAuth === false) next();
         else next({ path: "/" });
