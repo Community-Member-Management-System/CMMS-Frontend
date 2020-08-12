0b8e7ede3ec6c96a805ebc9ffdf61d3de8449123
Censyu <censyu@outlook.com>
2020-08-12 16:39:18 +0800

    Update layout of activity page #21

--- cmms/src/components/Activity/ActivityPage.vue
+++ cmms/src/components/Activity/ActivityPage.vue
@@ -6,20 +6,33 @@
         <v-card class="mb-5 pa-10">
           <v-row>
             <v-col cols="10">
-              <h1>{{ activity.title }}</h1>
+              <div class="text-h4">{{ activity.title }}</div>
             </v-col>
             <v-col cols="2">
-              <v-chip color="green" text-color="white">
+              <v-chip v-if="activity.status === '未开始'" color="blue" text-color="white">
+                <v-avatar left>
+                  <v-icon>mdi-calendar-clock</v-icon>
+                </v-avatar>未开始
+              </v-chip>
+              <v-chip v-if="activity.status === '进行中'" color="yellow darken-3" text-color="white">
+                <v-avatar left>
+                  <v-icon>mdi-clock-outline</v-icon>
+                </v-avatar>进行中
+              </v-chip>
+              <v-chip v-if="activity.status === '已结束'" color="green" text-color="white">
                 <v-avatar left>
                   <v-icon>mdi-checkbox-marked-circle</v-icon>
-                </v-avatar>
-                {{ activity.status }}
+                </v-avatar>已结束
               </v-chip>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col>时间：{{ activity.location }} 地点：{{ activity.location }}</v-col>
-          </v-row>
+          <v-card-text class="px-0 py-1">创建于：{{ activity.created_date }}</v-card-text>
+          <v-card-text class="px-0 py-1">活动时间：{{ activity.start_time }} - {{ activity.end_time }}</v-card-text>
+          <v-card-text class="px-0 py-1">
+            <!-- <v-icon>mid-map-marker</v-icon> -->
+            活动地点：{{ activity.location }}
+          </v-card-text>
+          <v-divider class="my-5"></v-divider>
           <markdown-it-vue-light class="md-body" :content="activity.description" />
         </v-card>
       </v-col>
