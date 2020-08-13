693ec7730fe26ca06147bde94f166812127762e3
Censyu <censyu@outlook.com>
2020-08-13 22:30:31 +0800

    Update layout for sign page

--- cmms/src/components/ActivitySignClub.vue
+++ cmms/src/components/ActivitySignClub.vue
@@ -1,32 +1,26 @@
 <template>
-  <v-container fluid>
-    <v-row align="center" justify="center" class="py-12 my-12">
-      <v-col cols="6">
-        <v-row>
-          <v-col v-for="(i, idx) in signCode" :key="idx">
-            <v-card class="my-auto">
-              <v-responsive :aspect-ratio="1/1">
-                <v-card-text class="py-auto font-weight-light display-4 text-center">
-                  {{ i }}
-                </v-card-text>
-              </v-responsive>
-            </v-card>
-          </v-col>
-          <!--                    <v-progress-linear value="15"></v-progress-linear>-->
-        </v-row>
+  <v-container>
+    <v-row align="center" justify="center" class="mx-auto py-12 my-12">
+      <v-col class="d-flex justify-center" cols="12" md="10">
+        <v-card class="d-inline-block mx-1 mx-sm-4 pa-2" v-for="(i, idx) in signCode" :key="idx">
+          <v-card-text class="pa-0 text-h2 text-sm-h1 text-center">{{ i }}</v-card-text>
+        </v-card>
       </v-col>
+      <!-- <v-progress-linear value="15"></v-progress-linear> -->
     </v-row>
-    <v-row align="center" justify="center" class="font-weight-weight display-1">
-      {{ clubName }} {{ activityName }} 签到
-    </v-row>
+    <v-row
+      class="text-h5 text-sm-h3"
+      align="center"
+      justify="center"
+    >{{ clubName }} {{ activityName }} 签到</v-row>
   </v-container>
 </template>
 
 <script>
-const jsotp = require('jsotp')
+const jsotp = require("jsotp");
 
 export default {
-  name: 'ActivitySignClub',
+  name: "ActivitySignClub",
 
   data: () => ({
     signCode: "1234",
@@ -35,22 +29,21 @@ export default {
     activityName: "无名活动",
     activityID: 1,
     otpKey: null,
-    otpInstance: null
+    otpInstance: null,
   }),
   methods: {},
   mounted() {
     this.axios
       .get("/api/activity/" + this.activityID + "/secret_key")
       .then((response) => {
-        this.otpKey = response.data['secret_key']
-        this.otpInstance = jsotp.TOTP(this.otpKey)
-      })
+        this.otpKey = response.data["secret_key"];
+        this.otpInstance = jsotp.TOTP(this.otpKey);
+      });
     this.$nextTick(function () {
       window.setInterval(() => {
-        this.signCode = this.otpInstance.now()
-      }, 3000)
-    })
-  }
-
-}
+        this.signCode = this.otpInstance.now();
+      }, 3000);
+    });
+  },
+};
 </script>
