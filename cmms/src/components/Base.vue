73eedf45432f60170ea8e36a5f3a37b1c3eb30bc
Censyu <censyu@outlook.com>
2020-08-13 17:18:50 +0800

    Update layout for notice

--- cmms/src/components/Base.vue
+++ cmms/src/components/Base.vue
@@ -101,18 +101,34 @@
         </v-btn>
 
         <!-- 通知 -->
-        <!-- <v-btn icon>
-          <v-icon>mdi-bell</v-icon>
-        </v-btn>-->
         <v-menu offset-y>
           <template v-slot:activator="{ on }">
             <v-btn color="rgba(0, 0, 0, 0)" depressed v-on="on">
-              <v-icon>mdi-bell</v-icon>
+              <v-badge color="red" dot>
+                <v-icon>mdi-bell</v-icon>
+              </v-badge>
             </v-btn>
           </template>
-          <v-card class="px-3">
+          <v-card class="mx-auto px-3" max-width="300">
             <v-list>
-              <v-divider></v-divider>
+              <v-list-item v-if="notice.length == 0">
+                <v-list-item-content>
+                  <v-list-item-subtitle>暂时没有通知</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <template v-else v-for="(item, index) in notice">
+                <v-list-item :key="item.pk">
+                  <v-list-item-content>
+                    <v-list-item-title>标题</v-list-item-title>
+                    <v-list-item-subtitle v-text="item.description"></v-list-item-subtitle>
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-list-item-action-text>1 min</v-list-item-action-text>
+                    <v-icon>close</v-icon>
+                  </v-list-item-action>
+                </v-list-item>
+                <v-divider v-if="index + 1 < notice.length" :key="index"></v-divider>
+              </template>
             </v-list>
           </v-card>
         </v-menu>
@@ -130,7 +146,7 @@
               <v-icon>mdi-menu-down</v-icon>
             </v-btn>
           </template>
-          <v-card class="px-3">
+          <v-card class="mx-auto px-3" max-width="300">
             <v-list>
               <v-list-item>
                 <v-list-item-avatar color="grey">
@@ -198,6 +214,7 @@ export default {
     fab: false,
     expand: false,
     query: "",
+    notice_status: [],
     notice: [],
     list: [
       {
@@ -256,28 +273,26 @@ export default {
     user() {
       let user = this.$store.getters.user;
       if (!user) {
-        user =  {
+        user = {
           avatar: null,
           nick_name: "",
           real_name: "",
           email: "",
           phone: "",
           profile: "",
-          communities: []
-        }
+          communities: [],
+        };
       }
-      return user
+      return user;
     },
   },
   methods: {
-    async fetchUserInfo() {
-      await this.axios
+    fetchUserInfo() {
+      this.axios
         .post("/api/auth/check", null, {
           headers: { "X-CSRFToken": this.$cookies.get("csrftoken") },
         })
         .then((response) => {
-          // console.log("Check User: ");
-          // console.log(response);
           let userID = response.data.userid.toString();
           let userNew = response.data.new;
           if (userNew) {
@@ -285,7 +300,77 @@ export default {
           }
           this.$store.dispatch("updateUser", userID);
         });
-      // this.user = this.$store.getters.user
+    },
+    fetchNoticeList() {
+      // this.axios
+      //   .get("/api/notice", null, {
+      //     headers: { "X-CSRFToken": this.$cookies.get("csrftoken") },
+      //   })
+      //   .then((response) => {
+      //     let userID = response.data.userid.toString();
+      //     let userNew = response.data.new;
+      //     if (userNew) {
+      //       this.$router.push("SetUserInfo");
+      //     }
+      //     this.$store.dispatch("updateUser", userID);
+      //   });
+    },
+    timeFormat(timestamp) {
+      let mistiming = Math.round((Date.now() - timestamp) / 1000);
+      let arrr = ["年", "个月", "星期", "天", "小时", "分钟", "秒"];
+      let arrn = [31536000, 2592000, 604800, 86400, 3600, 60, 1];
+      for (let i = 0; i < arrn.length; i++) {
+        let inm = Math.floor(mistiming / arrn[i]);
+        if (inm != 0) {
+          return inm + arrr[i] + "前";
+        }
+      }
+    },
+    getNoticeTitle(type, subtype) {
+      switch (type) {
+        case "PC":
+          switch (subtype) {
+            case 0:
+              return "被邀请加入社团";
+            case 1:
+              return "取消管理员";
+            case 2:
+              return "被踢出社团";
+          }
+        case "CA":
+          switch (subtype) {
+            case 0:
+              return "创建社团审核结果";
+            case 1:
+              return "加入社团审核结果";
+          }
+        case "B":
+          return "你的账户已被封禁";
+        case "C_AN":
+          switch (subtype) {
+            case 0:
+              return "活动创建";
+            case 1:
+              return "活动更新";
+            case 2:
+              return "活动删除";
+          }
+        case "C_AP":
+          switch (subtype) {
+            case 0:
+              return "社团管理员个人邀请被拒绝";
+            case 1:
+              return "社团成员权限变更";
+            case 2:
+              return "成员增减";
+          }
+        case "C_AA":
+          return "社团管理员审核用户加入请求";
+        case "C_D":
+          return "社团已被解散";
+        case "S_CA":
+          return "系统管理员审核社团创建请求";
+      }
     },
     onResize() {
       if (
