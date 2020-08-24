a5b7ec325c28f640be092d77fd3f58bf131d2a80
Censyu <censyu@outlook.com>
2020-08-24 22:00:02 +0800

    Update style & layout of Login page

--- cmms/src/components/Login.vue
+++ cmms/src/components/Login.vue
@@ -1,19 +1,57 @@
 <template>
-  <v-container class="fill-height">
-    <v-row class="mx-auto" align="center" justify="center">
-      <h1 class="text-center text-h2 col col-12 my-5">USTC | 社团</h1>
-      <v-btn class="mx-3" color="primary" rounded x-large outlined href="/api/auth/cas_login" id='cas_login'>
-        <v-icon>mdi-account</v-icon>统一身份验证登录
+  <v-app>
+    <v-app-bar class="background" flat>
+      <v-spacer></v-spacer>
+      <v-btn class="mx-auto" rounded outlined href="/api/auth/cas_login" id="cas_login">
+        <!-- <v-icon>mdi-account</v-icon> -->
+        统一身份验证登录
       </v-btn>
-      <v-btn class="mx-3" rounded x-large outlined link to="/home">[测试用] 直接进入</v-btn>
-    </v-row>
-  </v-container>
+    </v-app-bar>
+    <v-container class="fill-height background" fluid>
+      <v-row class="mx-auto" align="center" justify="center">
+        <v-col>
+          <div class="text-h2 text-center my-5">USTC | 社团</div>
+          <div class="text-h6 text-center mb-10">发现兴趣、寻找你喜欢的社团</div>
+          <v-text-field
+            class="search mx-auto"
+            rounded
+            filled
+            single-line
+            hide-details
+            autofocus
+            placeholder="搜索社团、活动、成员 ..."
+            append-icon="mdi-magnify"
+            @click:append="search"
+            @keydown.enter="search"
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-img class="ma-10" src="../assets/community.svg"></v-img>
+        </v-col>
+      </v-row>
+      <!-- <v-row class="mx-auto mb-5" align="center" justify="center">
+        <v-btn depressed><v-icon>mdi-chevron-double-down</v-icon>查看所有社团信息</v-btn>
+      </v-row> -->
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: "Login",
   data: () => ({}),
-  methods: { },
+  methods: {
+    search() {
+      this.$router.push({
+        path: "/search",
+        query: { q: this.query },
+      });
+    },
+  },
 };
 </script>
+<style scoped>
+.search {
+  max-width: 500px;
+}
+</style>
