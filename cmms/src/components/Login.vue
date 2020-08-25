65b19e68f77dc32028f8c0db9a5035b0193e0b86
Censyu <censyu@outlook.com>
2020-08-25 22:26:12 +0800

    Update layout of login

--- cmms/src/components/Login.vue
+++ cmms/src/components/Login.vue
@@ -1,45 +1,60 @@
 <template>
   <v-app>
-    <v-app-bar class="background" flat>
-      <v-spacer></v-spacer>
-      <v-btn class="mx-auto" rounded outlined href="/api/auth/cas_login" id="cas_login">
-        <!-- <v-icon>mdi-account</v-icon> -->
-        统一身份验证登录
-      </v-btn>
-    </v-app-bar>
     <v-container class="fill-height background" fluid>
-      <v-row class="mx-auto" align="center" justify="center">
-        <v-col>
-          <div class="text-h2 text-center my-5">USTC | 社团</div>
-          <div class="text-h6 text-center mb-10">发现兴趣、寻找你喜欢的社团</div>
-          <v-text-field
-            class="search mx-auto"
-            v-model="query"
-            rounded
-            filled
-            single-line
-            hide-details
-            autofocus
-            placeholder="搜索社团、活动、成员 ..."
-            append-icon="mdi-magnify"
-            @click:append="search"
-            @keydown.enter="search"
-          ></v-text-field>
-        </v-col>
-        <v-col>
-          <v-img class="ma-10" src="../assets/community.svg"></v-img>
-        </v-col>
-      </v-row>
-      <!-- <v-row class="mx-auto mb-5" align="center" justify="center">
-        <v-btn depressed><v-icon>mdi-chevron-double-down</v-icon>查看所有社团信息</v-btn>
-      </v-row>-->
+      <v-btn
+        class="mx-auto login-btn"
+        large
+        rounded
+        outlined
+        href="/api/auth/cas_login"
+        id="cas_login"
+      >统一身份验证登录</v-btn>
+      <v-tabs class="fill-height" background-color="#EEEEEE" right>
+        <v-tab>首页</v-tab>
+        <v-tab>社团</v-tab>
+        <v-tab>活动</v-tab>
+        <v-tab-item class="fill-height background">
+          <v-row class="mx-auto py-auto" align="center" justify="center">
+            <v-col>
+              <div class="text-h2 text-center my-5">USTC | 社团</div>
+              <div class="text-h6 text-center mb-10">发现兴趣、寻找你喜欢的社团</div>
+              <v-text-field
+                class="search mx-auto"
+                v-model="query"
+                rounded
+                filled
+                single-line
+                hide-details
+                autofocus
+                placeholder="搜索社团、活动、成员 ..."
+                append-icon="mdi-magnify"
+                @click:append="search"
+                @keydown.enter="search"
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              <v-img class="ma-10" src="../assets/community.svg"></v-img>
+            </v-col>
+          </v-row>
+        </v-tab-item>
+        <v-tab-item class="fill-height background">
+          <CommunityList></CommunityList>
+        </v-tab-item>
+        <v-tab-item class="fill-height background">
+          <ActivityList></ActivityList>
+        </v-tab-item>
+      </v-tabs>
     </v-container>
   </v-app>
 </template>
 
 <script>
+import CommunityList from "./CommunityList";
+import ActivityList from "./ActivityList";
+
 export default {
   name: "Login",
+  components: { CommunityList, ActivityList },
   data: () => ({
     query: "",
   }),
@@ -56,5 +71,11 @@ export default {
 <style scoped>
 .search {
   max-width: 500px;
+}
+.login-btn {
+  position: absolute;
+  top: 20px;
+  left: 35px;
+  z-index: 1;
 }
 </style>
